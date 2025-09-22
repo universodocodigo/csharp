48437dfa7d74b0957bf3b39323d639360defa640
universodocodigo <universodocodigo@outlook.com.br>
2025-09-22 12:26:41 -0300

    olamundostring

---
+++ olamundostrings.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+	string primeiroNome = ("Universo");
+	string segundoNome = ("do Codigo");
+	  
+	Console.WriteLine($"Ola {primeiroNome} {segundoNome}");
+  }
+}
